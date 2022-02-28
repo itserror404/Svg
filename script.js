bd0c41f2a3feda4a6c6782c53acd975d4ff0b08e
Maimuna Zaheer <68902873+itserror404@users.noreply.github.com>
2022-03-01 00:28:17 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let insideflower=document.getElementById("layer2")
+
+insideflower.addEventListener("click", ()=> {
+
+	document.getElementById("textbox").innerText= "You clicked the centre of flower!"
+
+})
+
